d2a78331199b7fe55347af31eddc0456940bfaed
Yuan-Yuan52 <sam05022002@gmail.com>
2026-06-01 21:39:21 +0800

    class8 filering and histogram equalization

--- main.cpp
+++ main.cpp
@@ -16,27 +16,24 @@ int main() {
     resize(img, resized, Size(), 0.2, 0.2); // 不要直接 in-place，先縮放到暫存再賦值回 img
     img = resized;
 
-    // BGR → HSV
-    Mat hsv;
-    cvtColor(img, hsv, COLOR_BGR2HSV);
 
-    // BGR → YUV
-    Mat yuv;
-    cvtColor(img, yuv, COLOR_BGR2YUV);
+    // 轉灰階
+    Mat gray;
+    cvtColor(img, gray, COLOR_BGR2GRAY);
+
+    // 一般 Histogram 等化
+    Mat equalized;
+    equalizeHist(gray, equalized);
+
+    // CLAHE（限制對比度的進階版）
+    Ptr<CLAHE> clahe = createCLAHE(2.0, Size(8, 8));
+    Mat clahe_result;
+    clahe->apply(gray, clahe_result);
 
     // 顯示
-    imshow("Original BGR", img);
-    imshow("HSV", hsv);
-    imshow("YUV", yuv);
-
-    // 印出同一個像素在不同色彩空間的值
-    Vec3b px_bgr = img.at<Vec3b>(100, 100);
-    Vec3b px_hsv = hsv.at<Vec3b>(100, 100);
-    Vec3b px_yuv = yuv.at<Vec3b>(100, 100);
-
-    cout << "BGR: " << (int)px_bgr[0] << " " << (int)px_bgr[1] << " " << (int)px_bgr[2] << endl;
-    cout << "HSV: " << (int)px_hsv[0] << " " << (int)px_hsv[1] << " " << (int)px_hsv[2] << endl;
-    cout << "YUV: " << (int)px_yuv[0] << " " << (int)px_yuv[1] << " " << (int)px_yuv[2] << endl;
+    imshow("Original Gray", gray);
+    imshow("Equalized", equalized);
+    imshow("CLAHE", clahe_result);
 
     waitKey(0);
     return 0;
