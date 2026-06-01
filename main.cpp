eb5b3c0e72305394b2ac0065cf7bfdd081d92cc3
Yuan-Yuan52 <sam05022002@gmail.com>
2026-06-01 23:14:45 +0800

    class9 : PSNR in different BLUR

--- main.cpp
+++ main.cpp
@@ -4,6 +4,29 @@
 using namespace std;
 using namespace cv;
 
+Mat addGaussianNoise(Mat img, double variance) {
+    Mat result;
+    img.convertTo(result, CV_64F);  // 先轉成 64F
+
+    // 建立和 img 相同大小和通道數的噪聲
+    Mat noise(img.size(), CV_64F);
+    if (img.channels() == 3) {
+        Mat noise_3ch;
+        randn(noise, 0, sqrt(variance) * 255);
+        vector<Mat> channels = {noise, noise, noise};
+        merge(channels, noise_3ch);
+        result += noise_3ch;
+    } else {
+        randn(noise, 0, sqrt(variance) * 255);
+        result += noise;
+    }
+
+    cv::threshold(result, result, 0, 0, THRESH_TOZERO);
+    cv::threshold(result, result, 255, 255, THRESH_TRUNC);
+    result.convertTo(result, CV_8U);
+    return result;
+}
+
 int main() {
 
     Mat img = imread("C:\\Users\\user\\Desktop\\IMG_2024.jpg");
@@ -16,25 +39,24 @@ int main() {
     resize(img, resized, Size(), 0.2, 0.2); // 不要直接 in-place，先縮放到暫存再賦值回 img
     img = resized;
 
+    Mat noisy = addGaussianNoise(img, 0.01); // 添加高斯噪声，方差为0.01
 
-    // 轉灰階
-    Mat gray;
-    cvtColor(img, gray, COLOR_BGR2GRAY);
+    Mat mean_filtered,gaussian_blured,median_filtered;
+    blur(noisy, mean_filtered, Size(5, 5)); // 均值滤波
+    GaussianBlur(noisy, gaussian_blured, Size(7, 7), 4); // 高斯滤波
+    medianBlur(noisy, median_filtered, 7); // 中值滤波
 
-    // 一般 Histogram 等化
-    Mat equalized;
-    equalizeHist(gray, equalized);
+    cout << "Noisy PSNR: " << PSNR(img, noisy) << " dB" << endl;
+    cout << "Mean Filter PSNR: " << PSNR(img, mean_filtered) << " dB" << endl;
+    cout << "Gaussian Blur PSNR: " << PSNR(img, gaussian_blured) << " dB" << endl;
+    cout << "Median Filter PSNR: " << PSNR(img, median_filtered) << " dB" << endl;  
 
-    // CLAHE（限制對比度的進階版）
-    Ptr<CLAHE> clahe = createCLAHE(2.0, Size(8, 8));
-    Mat clahe_result;
-    clahe->apply(gray, clahe_result);
-
-    // 顯示
-    imshow("Original Gray", gray);
-    imshow("Equalized", equalized);
-    imshow("CLAHE", clahe_result);
-
+    imshow("Original Image", img);
+    imshow("Noisy Image", noisy);
+    imshow("Mean Filtered Image", mean_filtered);
+    imshow("Gaussian Blurred Image", gaussian_blured);
+    imshow("Median Filtered Image", median_filtered);
     waitKey(0);
     return 0;
+
 }
